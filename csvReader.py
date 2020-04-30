2e7197a0245b667b04c27bba5255ad57acf228a0
Alejandro Matos Mercado <alejandro,matos1@upr.edu>
2020-04-30 18:11:43 -0400

    Added csv reader

---
+++ csvReader.py
@@ -0,0 +1,14 @@
+import csv
+
+class CSVReader:
+
+    def __init__(self):
+        super().__init__()
+
+    def getData(self):
+        with open('maps/map1.csv') as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            result = []
+            for row in readCSV:
+                result.append(row)
+            print(result[1:])
---
+++ main.py
@@ -0,0 +1,7 @@
+from csvReader import CSVReader
+
+def main():
+    CSVReader().getData()
+
+if __name__ == "__main__":
+    main()
